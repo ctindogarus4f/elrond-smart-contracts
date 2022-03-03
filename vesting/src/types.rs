be4adb16b35391dc8b7142fa7e1cd3de186c5d9f
Razvan Angheluta <razvan@mocapp.net>
2022-03-03 23:26:50 +0200

    groupinfo added + restrict endpoints permission

--- vesting/src/types.rs
+++ vesting/src/types.rs
@@ -4,10 +4,22 @@ elrond_wasm::derive_imports!();
 
 #[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
 pub struct BeneficiaryInfo<M: ManagedTypeApi> {
+    pub group_type: GroupType,
+
     pub start: u64, // start of the vesting scheme
+    pub tokens_allocated: BigUint<M>,
+    pub tokens_claimed: BigUint<M>,
+}
+
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct GroupInfo {
     pub release_cliff: u64,
     pub release_percentage: u64,
     pub release_duration: u64,
-    pub tokens_allocated: BigUint<M>,
-    pub tokens_claimed: BigUint<M>,
+}
+
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub enum GroupType {
+    Developer,
+    Investor,
 }
