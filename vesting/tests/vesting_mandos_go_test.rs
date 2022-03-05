64b197ba27def42134b61c7873d9359b3ab48721
Razvan Angheluta <razvan@mocapp.net>
2022-03-05 16:19:53 +0200

    named arguments and name nested expected output/events

--- vesting/tests/vesting_mandos_go_test.rs
+++ vesting/tests/vesting_mandos_go_test.rs
@@ -1,9 +1,14 @@
 #[test]
+fn init_vesting() {
+    elrond_wasm_debug::mandos_go("mandos/init_vesting.scen.json");
+}
+
+#[test]
 fn group_info_go() {
     elrond_wasm_debug::mandos_go("mandos/group_info.scen.json");
 }
 
 #[test]
-fn init_vesting() {
-    elrond_wasm_debug::mandos_go("mandos/init_vesting.scen.json");
+fn beneficiary_info() {
+    elrond_wasm_debug::mandos_go("mandos/beneficiary_info.scen.json");
 }
