f677237e6984b08d5e7c6735ce6c98721307e9f5
Razvan Angheluta <razvan@mocapp.net>
2022-03-05 12:53:43 +0200

    removed unused files from test/

--- vesting/tests/vesting_mandos_go_test.rs
+++ vesting/tests/vesting_mandos_go_test.rs
@@ -1,4 +1,9 @@
 #[test]
-fn vesting_go() {
-    elrond_wasm_debug::mandos_go("mandos/vesting.scen.json");
+fn group_info_go() {
+    elrond_wasm_debug::mandos_go("mandos/group_info.scen.json");
+}
+
+#[test]
+fn init_vesting() {
+    elrond_wasm_debug::mandos_go("mandos/init_vesting.scen.json");
 }
