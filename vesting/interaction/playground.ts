3e257820464f26daa1867f9b508b7b0db79da8d1
ctindogarus4f <constantin.dogaru@sense4fit.com>
2022-03-16 12:37:39 +0200

    query the vesting contract and get the group info

--- vesting/interaction/playground.ts
+++ vesting/interaction/playground.ts
@@ -1,6 +1,8 @@
 import {
+  AbiRegistry,
   Account,
   Address,
+  BinaryCodec,
   UserSigner,
   ContractFunction,
   GasLimit,
@@ -12,6 +14,7 @@ import {
 } from "@elrondnetwork/erdjs";
 const fs = require("fs");
 
+const ABI_PATH = "../output/vesting.abi.json";
 const GAS_LIMIT = 600000000;
 const OWNER_WALLET = "/Users/constantindogaru/elrond-wallet/wallet-owner.pem";
 const PROXY = "https://devnet-api.elrond.com";
@@ -19,6 +22,11 @@ const VESTING_SC_ADDRESS =
   "erd1qqqqqqqqqqqqqpgqfh8txltrewpf6rrec7ntzfw682gdy98d8x5qcq49t2";
 
 const main = async () => {
+  // CODEC SETUP
+  let abi = await AbiRegistry.load({ files: [ABI_PATH] });
+  let groupInfoType = abi.getStruct("GroupInfo");
+  let codec = new BinaryCodec();
+
   // NETWORK SETUP
   const provider = new ProxyProvider(PROXY);
   await NetworkConfig.getDefault().sync(provider);
@@ -57,15 +65,26 @@ const main = async () => {
       ],
     });
 
-    console.log(`Adding group ${name}`);
+    console.log(`## Adding group ${name}`);
     tx.setNonce(owner.nonce);
     owner.incrementNonce();
     await signer.sign(tx);
     await tx.send(provider);
     await tx.awaitExecuted(provider);
     console.log(
-      `Successfully added group ${name} via transaction with hash ${tx.getHash()}`,
+      `## Successfully added group ${name} via transaction with hash ${tx.getHash()}`,
     );
+
+    console.log(`## Fetching group ${name}`);
+    let response = await contract.runQuery(provider, {
+      func: new ContractFunction("getGroupInfo"),
+      args: [new U8Value(id)],
+    });
+
+    let decodedResponse = codec
+      .decodeTopLevel(response.outputUntyped()[0], groupInfoType)
+      .valueOf();
+    console.log(decodedResponse);
   }
 };
 
