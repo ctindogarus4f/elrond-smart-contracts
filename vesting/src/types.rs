79116f50f6c53093255e7c49c8029f67ef7cd9ab
ctindogarus4f <constantin.dogaru@sense4fit.com>
2022-03-17 11:50:29 +0200

    add comments to the structs

--- vesting/src/types.rs
+++ vesting/src/types.rs
@@ -4,12 +4,12 @@ elrond_wasm::derive_imports!();
 
 #[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
 pub struct BeneficiaryInfo<M: ManagedTypeApi> {
-    pub can_be_revoked: bool,
-    pub is_revoked: bool,
-    pub group_type: GroupType,
-    pub start: u64,                   // start of the vesting scheme
+    pub can_be_revoked: bool, // true if the beneficiary can be removed from the vesting scheme. false otherwise
+    pub is_revoked: bool, // true if the beneficiary was removed from the vesting scheme. false otherwise
+    pub group_type: GroupType, // group type that the beneficiary falls into (ex: Team)
+    pub start: u64,       // start of the vesting scheme as a unix timestamp
     pub tokens_allocated: BigUint<M>, // amount of tokens allocated to this beneficiary
-    pub tokens_claimed: BigUint<M>,   // amount of tokens claimed by this beneficiary
+    pub tokens_claimed: BigUint<M>, // amount of tokens claimed by this beneficiary
 }
 
 #[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
@@ -26,7 +26,7 @@ pub enum GroupType {
     Marketing,       // we will have a multisig address for this type - id 1
     PrivateInvestor, // we will have multiple beneficiaries for this type - id 2
     SeedInvestor,    // we will have multiple beneficiaries for this type - id 3
-    Strategy,        // we will have a single beneficiary for this type - id 4
+    Strategy,        // we will have a multisig address for this type - id 4
     Team,            // we will have multiple beneficiaries for this type - id 5
     Treasury,        // we will have a single beneficiary for this type - id 6
 }
