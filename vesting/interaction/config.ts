4cb5fbe85bc7cbf0837d26a56fdd151d53362c6e
ctindogarus4f <constantin.dogaru@sense4fit.com>
2022-03-20 18:56:43 +0200

    change config with the new vesting contract

--- vesting/interaction/config.ts
+++ vesting/interaction/config.ts
@@ -4,4 +4,4 @@ export const OWNER_WALLET =
   "/Users/constantindogaru/elrond-wallet/wallet-owner.pem";
 export const PROXY = "https://devnet-api.elrond.com";
 export const VESTING_SC_ADDRESS =
-  "erd1qqqqqqqqqqqqqpgqzt8j6jz3g32qh76rvxutxsz4qajennee8x5qvyv0zk";
+  "erd1qqqqqqqqqqqqqpgqwk3v99ezyalv8vx9cv2khzme6yft4eyf8x5q2rszls";
