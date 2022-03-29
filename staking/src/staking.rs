585655f53ddd47d90a4a642adb9f384141cb9c32
ctindogarus4f <constantin.dogaru@sense4fit.com>
2022-03-29 12:21:16 +0300

    add token identifier in the init

--- staking/src/staking.rs
+++ staking/src/staking.rs
@@ -6,5 +6,17 @@ elrond_wasm::imports!();
 #[elrond_wasm::derive::contract]
 pub trait StakingContract {
     #[init]
-    fn init(&self) {}
+    fn init(&self, token_identifier: TokenIdentifier) {
+        require!(
+            token_identifier.is_valid_esdt_identifier(),
+            "invalid esdt token"
+        );
+        self.token_identifier().set_if_empty(&token_identifier);
+    }
+
+    // storage
+
+    #[view(getTokenIdentifier)]
+    #[storage_mapper("tokenIdentifier")]
+    fn token_identifier(&self) -> SingleValueMapper<TokenIdentifier>;
 }
