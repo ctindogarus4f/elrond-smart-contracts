a60c68d28e1c4e5d1296181f23c96f532ddbf193
ctindogarus4f <constantin.dogaru@sense4fit.com>
2022-04-05 16:59:07 +0300

    change devnet with testnet inside the config

--- vesting/interaction/config.ts
+++ vesting/interaction/config.ts
@@ -3,6 +3,6 @@ export const DECIMALS_SUFFIX = ",000,000,000,000,000,000";
 export const GAS_LIMIT = 600000000;
 export const OWNER_WALLET =
   "/Users/constantindogaru/elrond-wallet/wallet-owner.pem";
-export const PROXY = "https://devnet-api.elrond.com";
+export const PROXY = "https://testnet-api.elrond.com";
 export const VESTING_SC_ADDRESS =
-  "erd1qqqqqqqqqqqqqpgqx8fqsl0fudvwmakmjtmpj6zeqjepenrn8x5qxuxn5y";
+  "erd1qqqqqqqqqqqqqpgqc3k0cqares9ejj26pfqhgfqklammpy208x5qa83dzd";
