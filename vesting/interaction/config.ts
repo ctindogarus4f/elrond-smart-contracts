374124350d3132b0d8412cec3e91cd12f7f707e5
Constantin Dogaru <constantin.dogaru@sense4fit.com>
2023-02-01 11:00:00 +0200

    change testnet to mainnet

--- vesting/interaction/config.ts
+++ vesting/interaction/config.ts
@@ -3,9 +3,9 @@ export const DECIMALS_SUFFIX = ",000,000,000,000,000,000";
 export const REGULAR_GAS_LIMIT = 60_000_000;
 export const MAX_GAS_LIMIT = 600_000_000;
 export const OWNER_WALLET =
-  "/Users/constantindogaru/elrond-wallet/wallet-owner.pem";
-export const EXPLORER = "https://testnet-explorer.multiversx.com";
-export const PROXY = "https://testnet-gateway.multiversx.com";
-export const CHAIN_ID = "T";
+  "/Users/constantindogaru/elrond-wallet/wallet-owner-v.pem";
+export const EXPLORER = "https://explorer.multiversx.com";
+export const PROXY = "https://gateway.multiversx.com";
+export const CHAIN_ID = "1";
 export const VESTING_SC_ADDRESS =
-  "erd1qqqqqqqqqqqqqpgqrv52hpzyrsg4rrws6jj4hlx7gsrg52eszjcq3lksca";
+  "erd1qqqqqqqqqqqqqpgq72cxkseceh385txwmuurn5fqy4jvch2jup4s4xscn4";
