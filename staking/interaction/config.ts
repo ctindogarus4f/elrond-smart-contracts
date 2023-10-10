c0db1593fa2ae465cfd027963964bae77afb8da5
Constantin Dogaru <constantin.dogaru@sense4fit.com>
2023-10-10 14:22:14 -0700

    add new stake packages

--- staking/interaction/config.ts
+++ staking/interaction/config.ts
@@ -4,8 +4,8 @@ export const REGULAR_GAS_LIMIT = 60_000_000;
 export const MAX_GAS_LIMIT = 600_000_000;
 export const OWNER_WALLET =
   "/Users/constantindogaru/elrond-wallet/wallet-owner-s.pem";
-export const EXPLORER = "https://explorer.elrond.com";
-export const PROXY = "https://gateway.elrond.com";
+export const EXPLORER = "https://explorer.multiversx.com";
+export const PROXY = "https://gateway.multiversx.com";
 export const CHAIN_ID = "1";
 export const STAKING_SC_ADDRESS =
   "erd1qqqqqqqqqqqqqpgquef97h66mjmgghdglpkjllamt2hjz2zcaykqpwwurh";
