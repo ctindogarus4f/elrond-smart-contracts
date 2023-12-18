6f6e129107d9b83152c05b7d7871720b608107d5
Constantin Dogaru <constantin.dogaru@sense4fit.com>
2023-12-18 12:27:03 +0200

    add dao boilerplate

--- dao/src/dao.rs
+++ dao/src/dao.rs
@@ -1,10 +1,10 @@
 #![no_std]
 
-elrond_wasm::imports!();
+multiversx_sc::imports!();
 
 /// An empty contract. To be used as a template when starting a new contract from scratch.
-#[elrond_wasm::derive::contract]
-pub trait EmptyContract {
+#[multiversx_sc::contract]
+pub trait Dao {
     #[init]
     fn init(&self) {}
 }
