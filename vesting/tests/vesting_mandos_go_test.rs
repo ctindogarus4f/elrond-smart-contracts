3a9b55ad4ce47afc04f32d2edf55ae5f0dedcddf
Constantin Dogaru <constantin.dogaru@sense4fit.com>
2024-02-13 17:54:52 +0200

    correct tests

--- vesting/tests/vesting_mandos_go_test.rs
+++ vesting/tests/vesting_mandos_go_test.rs
@@ -6,20 +6,20 @@ fn world() -> ScenarioWorld {
 
 #[test]
 fn setup() {
-    world().run("mandos/setup.scen.json");
+    world().run("scenarios/setup.scen.json");
 }
 
 #[test]
 fn group_info() {
-    world().run("mandos/group_info.scen.json");
+    world().run("scenarios/group_info.scen.json");
 }
 
 #[test]
 fn beneficiary_info() {
-    world().run("mandos/beneficiary_info.scen.json");
+    world().run("scenarios/beneficiary_info.scen.json");
 }
 
 #[test]
 fn vesting_logic() {
-    world().run("mandos/vesting_logic.scen.json");
+    world().run("scenarios/vesting_logic.scen.json");
 }
--- vesting/tests/vesting_mandos_rs_test.rs
+++ vesting/tests/vesting_mandos_rs_test.rs
@@ -2,26 +2,27 @@ use multiversx_sc_scenario::*;
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
+
     blockchain.register_contract("file:output/vesting.wasm", vesting::ContractBuilder);
     blockchain
 }
 
 #[test]
 fn setup() {
-    world().run("mandos/setup.scen.json");
+    world().run("scenarios/setup.scen.json");
 }
 
 #[test]
 fn group_info() {
-    world().run("mandos/group_info.scen.json");
+    world().run("scenarios/group_info.scen.json");
 }
 
 #[test]
 fn beneficiary_info() {
-    world().run("mandos/beneficiary_info.scen.json");
+    world().run("scenarios/beneficiary_info.scen.json");
 }
 
 #[test]
 fn vesting_logic() {
-    world().run("mandos/vesting_logic.scen.json");
+    world().run("scenarios/vesting_logic.scen.json");
 }
